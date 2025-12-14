a6d9147126792d57365c59663e74b5851c417211
Gololob <gololoboffegor@gmail.com>
2025-12-14 15:38:00 +0300

    d

--- Snake_console_version/Snake_console_version.cpp
+++ Snake_console_version/Snake_console_version.cpp
@@ -1,4 +1,4 @@
-// Snake_console_version.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Snake_console_version.cpp : Tle contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
